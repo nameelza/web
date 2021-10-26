49d8bbf9a24adee51d8a28b41587e9d91cacc8af
nameelza <82921161+nameelza@users.noreply.github.com>
2021-10-26 14:25:26 -0700

    entry

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    entry = util.get_entry(title)
+    if entry:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": markdown2.markdown(entry)
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error": "Entry not found"
+        })
