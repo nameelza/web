5e3698bfbda614a838399590c184a7652df969ee
nameelza <82921161+nameelza@users.noreply.github.com>
2021-10-28 09:45:19 -0700

    edit page done

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("new", views.new, name="new"),
-    path("edit", views.edit, name="edit")
+    path("<str:title>/edit", views.edit, name="edit")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,47 +4,61 @@ from django.shortcuts import render
 from . import util
 
 import markdown2
-
+from random import random
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
-def entry(request, title):
-    entry = util.get_entry(title)
-    if entry:
-        return render(request, "encyclopedia/entry.html", {
-            "title": title,
-            "entry": markdown2.markdown(entry)
+    if request.method == "GET":
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
         })
     else:
         return render(request, "encyclopedia/error.html", {
-            "error": "Entry not found"
+            "error": "Invalid request"
+        })
+
+def entry(request, title):
+    if request.method == "GET":
+        entry = util.get_entry(title)
+        if entry:
+            return render(request, "encyclopedia/entry.html", {
+                "title": title,
+                "entry": markdown2.markdown(entry)
+            })
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "error": "Entry not found"
+            })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "error": "Invalid request"
         })
 
 def search(request):
-    query = request.GET.get("q").lower()
-    entries = util.list_entries()
-    results = []
-    for ent in entries:
-        if query == ent.lower():
-            return entry(request, query)
-        elif query in ent.lower():
-            results.append(ent)
-    if len(results) == 0:
-        return render(request, "encyclopedia/error.html", {
-            "error": "No results found"
-        })
+    if request.method == "GET":
+        query = request.GET.get("q").lower()
+        entries = util.list_entries()
+        results = []
+        for ent in entries:
+            if query == ent.lower():
+                return entry(request, query)
+            elif query in ent.lower():
+                results.append(ent)
+        if len(results) == 0:
+            return render(request, "encyclopedia/error.html", {
+                "error": "No results found"
+            })
+        else:
+            return render(request, "encyclopedia/index.html", {
+                "entries": results,
+                "message": f"Search results for '{query}'"
+            })
     else:
-        return render(request, "encyclopedia/index.html", {
-            "entries": results,
-            "message": f"Search results for '{query}'"
+        return render(request, "encyclopedia/error.html", {
+            "error": "Invalid request"
         })
-            
+
 def new(request):
     if request.method =="POST":
-
         title = request.POST.get("title").capitalize()
         content = request.POST.get("content")
         wholeContent = f"# {title}\n\n{content}"
@@ -65,11 +79,18 @@ def new(request):
             "error": "Invalid request"
         })
 
-def edit(request):
+def edit(request, title):
     if request.method =="POST":
-        pass
+        content = request.POST.get("content")
+        print(content)
+        util.save_entry(title, content)
+        return entry(request, title)
     elif request.method == "GET":
-        return render(request,  "encyclopedia/editpage.html")
+        content = util.get_entry(title)
+        return render(request,  "encyclopedia/editpage.html", {
+            "title": title,
+            "content": content
+        })
     else:
         return render(request, "encyclopedia/error.html", {
             "error": "Invalid request"
