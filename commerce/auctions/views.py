187fa15c99cb2168590ed93f7cfd87cc6d8755e1
nameelza <82921161+nameelza@users.noreply.github.com>
2021-11-04 16:41:36 -0700

    listings

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -11,7 +11,7 @@ from .models import User, Listings, Bids
 
 
 def index(request):
-    listings = Listings.objects.all()
+    listings = Listings.objects.exclude(user=request.user).order_by('-created_at')
     return render(request, "auctions/index.html", {
         "listings": listings
     })
