9cf4eb1be9ae79ccea26d4222d9b404623985e0a
nameelza <82921161+nameelza@users.noreply.github.com>
2021-11-09 11:40:01 -0800

    mail js

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -20,6 +20,30 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // Compose button sends the email
+  document.querySelector('#compose-form').onsubmit = function () {
+    const recipients = document.querySelector('#compose-recipients');
+    const subject = document.querySelector('#compose-subject');
+    const body = document.querySelector('#compose-body');
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: recipients.value,
+          subject: subject.value,
+          body: body.value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+        console.log(result.message);
+        // Redirect to sent view
+        load_mailbox('sent');
+    });
+  }
+
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +54,59 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  if (mailbox == 'inbox') {
+    fetch('/emails/inbox')
+    .then(response => response.json())
+    .then(emails => {
+        // Print emails
+        console.log(emails);
+        // ... do something else with emails ...
+        emails.forEach(email => {
+          document.querySelector('#emails-view').innerHTML += `
+            <div class="email">
+              <div class="sender">${email.sender}</div>
+              <div class="subject">${email.subject}</div>
+              <div class="timestamp">${email.timestamp}</div>
+            </div>
+          `;
+        });
+    });
+
+  } else if (mailbox == 'sent') {
+    fetch('/emails/sent')
+    .then(response => response.json())
+    .then(emails => {
+        // Print emails
+        console.log(emails);
+        // ... do something else with emails ...
+        emails.forEach(email => {
+          document.querySelector('#emails-view').innerHTML += `
+            <div class="email">
+              <div class="sender">${email.recipients}</div>
+              <div class="subject">${email.subject}</div>
+              <div class="timestamp">${email.timestamp}</div>
+            </div>
+          `;
+        });
+    });
+
+  } else if (mailbox == 'archive') {
+    fetch('/emails/archive')
+    .then(response => response.json())
+    .then(emails => {
+        // Print emails
+        console.log(emails);
+        // ... do something else with emails ...
+        emails.forEach(email => {
+          document.querySelector('#emails-view').innerHTML += `
+            <div class="email">
+              <div class="sender">${email.sender}</div>
+              <div class="subject">${email.subject}</div>
+              <div class="timestamp">${email.timestamp}</div>
+            </div>
+          `;
+        });
+    });
+  }
 }
