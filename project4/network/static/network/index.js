04ff39ad71579b8c1446da613372a69ae2547493
nameelza <82921161+nameelza@users.noreply.github.com>
2021-11-24 16:22:43 -0800

    js likes

---
+++ project4/network/static/network/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // like button
+    likeButton = document.getElementById('likeIcon');
+    likeButton.addEventListener('click', function () {
+        // Change svg to filled heart
+        // Increment like count
+
+    });
+});
