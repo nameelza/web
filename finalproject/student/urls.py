0d31f49f9a8df43f5c1f9f073a5f42bbb328f7f9
nameelza <82921161+nameelza@users.noreply.github.com>
2021-12-13 16:53:48 -0800

    index&result pages UI

--- finalproject/student/urls.py
+++ finalproject/student/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("list_results", views.list_results, name="list_results"),
+    path("map_results",  views.map_results, name="map_results"),
+    path("rental", views.rental, name="rental"),
+    path("profile", views.profile, name="profile"),
+    path("create", views.create, name="create")
 ]
--- finalproject/student/views.py
+++ finalproject/student/views.py
@@ -5,3 +5,18 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'student/index.html')
+
+def list_results(request):
+    return render(request, 'student/results.html')
+
+def map_results(request):
+    return render(request, 'student/map.html')
+
+def rental(request):
+    return render(request, 'student/rental.html')
+
+def profile(request):
+    return render(request, 'student/profile.html')
+
+def create(request):
+    return render(request, 'student/create.html')
